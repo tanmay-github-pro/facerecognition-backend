123bd7e07c589b9d5465e252e4af66189f9d95ac
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-29 18:02:27 +0530

    changed database configuration

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'Tanmay@postgres',
-    database : 'faceRecogDB'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
