1612cd932668c6c1d82fd1fd7e9717a508ad4f44
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-29 17:06:56 +0530

    customised to my database

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    user : 'postgres',
+    password : 'Tanmay@postgres',
+    database : 'faceRecogDB'
   }
 });
 
@@ -32,6 +32,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
